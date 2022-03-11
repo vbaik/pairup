4b45b6184a43bd9603a8e5139f70db0bd3b0651f
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-10 22:50:18 -0400

    drive or navigate dropdown menu added

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,15 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
 
-export const Home = (props) => {
-  // console.log(React.version);
-  const { username } = props;
-  return (
-    <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDriver: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { username } = this.props;
+    console.log(this.state.isDriver);
+    return (
+      <div>
+        <h3>Welcome, {username}</h3>
+        <p>Would you like to drive or navigate?</p>
+        <select
+          className="driver-dropdown"
+          name="isDriver"
+          onChange={this.handleChange}
+          value={this.state.isDriver}
+        >
+          <option value={true}>Select One</option>
+          <option value={true}>Drive</option>
+          <option value={false}>Navigate</option>
+        </select>
+      </div>
+    );
+  }
+}
 
 const mapState = (state) => {
   return {
