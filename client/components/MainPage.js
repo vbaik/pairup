b2c5f4e7eaa84afdbde718a5e690207e800e808c
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-12 12:36:03 -0400

    started MUI: started to set up nav bar

--- client/components/MainPage.js
+++ client/components/MainPage.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+//style
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
 export default class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Find Your Perfect Pair-programming Partner</h1>
-        <img className="img-main" src="/background.png" />
+        <Box>
+          <Typography color="primary" variant="h2">
+            Find Your Perfect Pair-programming Partner
+          </Typography>
+          <img className="img-main" src="/background.png" />
 
-        {/* <Button variant="contained" color="primary">
+          {/* <Button variant="contained" color="primary">
           Hello World
         </Button> */}
+        </Box>
       </div>
     );
   }
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,33 +3,110 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 
-const Navbar = ({ handleClick, isLoggedIn, userImg }) => (
-  <div>
-    <Link to="/">
-      <h1>PairUp</h1>
-    </Link>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Find Partner</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-      <Link to="/users/profile">
-        <img src={userImg} />
-      </Link>
-    </nav>
-  </div>
-);
+//+++ style +++
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+const settings = ["Profile", "Logout"];
+
+const Navbar = ({ handleClick, isLoggedIn, userImg }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Link to="/">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              PairUp
+            </Typography>
+          </Link>
+
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+
+              <Link to="/home">
+                <Button color="inherit">Login</Button>
+              </Link>
+              <a href="#" onClick={handleClick}>
+                <Button color="inherit">Logout</Button>
+              </a>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">
+                <Button color="inherit">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button color="inherit">Sign Up</Button>
+              </Link>
+            </div>
+          )}
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <Link to="/users/profile">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src={userImg} />
+                </IconButton>
+              </Link>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 /**
  * CONTAINER
