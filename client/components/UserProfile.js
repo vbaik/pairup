ddb7fb048940b3531363b918b641f530fd16dbf7
Vivian Baik <59579994+vbaik@users.noreply.github.com>
2022-03-13 16:08:41 -0300

    mui dropdown working with mbti and updates backend db

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -15,7 +15,28 @@ import {
   Button,
   Grid,
   Avatar,
+  TextField,
+  Select,
 } from "@mui/material";
+
+const mbtiArr = [
+  "ISFJ",
+  "ESFJ",
+  "ISTJ",
+  "ISFP",
+  "ESTJ",
+  "ESFP",
+  "ENFP",
+  "ISTP",
+  "INFP",
+  "ESTP",
+  "INTP",
+  "ENTP",
+  "ENFJ",
+  "INTJ",
+  "ENTJ",
+  "INFJ",
+];
 
 class UserProfile extends React.Component {
   constructor(props) {
@@ -23,14 +44,15 @@ class UserProfile extends React.Component {
     this.state = {
       username: props.singleUser.username || "",
       password: props.singleUser.password || "",
-      mbtiId: "",
-      // level: "",
+      mbtiId: props.singleUser.mbtiId || "",
+      level: props.singleUser.level || "Beginner",
       // imageURL: "",
       // aboutMe: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.createMbtiMenu = this.createMbtiMenu.bind(this);
   }
   componentDidMount() {
     this.props.loadInitialData();
@@ -50,40 +72,67 @@ class UserProfile extends React.Component {
     this.props.updateUser({ ...this.state });
   }
 
+  createMbtiMenu(i, mbtiName) {
+    return (
+      <MenuItem key={i} value={i + 1}>
+        {mbtiName}
+      </MenuItem>
+    );
+  }
+
   render() {
     console.log("this. state >>>>>>>", this.state);
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Username: </label>
-          <input
-            name="username"
-            onChange={this.handleChange}
-            value={this.state.username}
-          />
-          <label>Password: </label>
-          <input
-            name="password"
-            onChange={this.handleChange}
-            value={this.state.password}
-          />
-          <p>Current MBTI: </p>
-          <label>Update MBTI: </label>
-          <input
-            name="mbtiId"
-            onChange={this.handleChange}
-            value={this.state.mbtiId}
-          />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              margin="normal"
+              name="username"
+              label="New Username"
+              onChange={this.handleChange}
+              value={this.state.username}
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              name="password"
+              label="New Password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+            <Typography>Update MBTI: </Typography>
+            {/* <TextField
+              name="mbtiId"
+              onChange={this.handleChange}
+              value={this.state.mbtiId}
+            /> */}
+            <Select
+              value={this.state.mbtiId}
+              label="Update Your MBTI"
+              onChange={this.handleChange}
+              name="mbtiId"
+            >
+              {mbtiArr.map((mbti, idx) => this.createMbtiMenu(idx, mbti))}
+            </Select>
 
-          <div className="submit-cancel-btn">
-            <button className="btn-submit" type="submit">
-              SAVE
-            </button>
-            <Link to="/home">
-              <button className="btn-cancel">CANCEL</button>
-            </Link>
-          </div>
-        </form>
+            <div className="submit-cancel-btn">
+              <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+                SAVE
+              </Button>
+              <Link to="/home">
+                <Button>CANCEL</Button>
+              </Link>
+            </div>
+          </form>
+        </Box>
       </div>
     );
   }
